b7d70e02b58db26668d267a0aa165b3d0bf9597d
Icaro-Cesar <icarocesar630@gmail.com>
2020-07-12 12:27:35 -0300

    Add a script in PowerShell

---
+++ Automation_AddUserAD.ps1
@@ -0,0 +1,22 @@
+﻿Function Import-ecorp
+    {
+    Param (
+        [string] $OUName
+        )
+
+    $ExportName = "C:\DataFiles\"+$OUName+".csv"
+    $Location = "OU="+$OUName+",DC=ecorp,DC=com"
+
+    New-ADOrganizationalUnit -Name $OUName -Path "DC=ecorp,DC=com" -ProtectedFromAccidentalDeletion $False
+
+    $LCPassword = "D3f4u1t@01" | ConvertTo-SecureString -AsPlainText -Force
+
+    Import-CSV $ExportName    |
+    New-ADUser -Path $Location -AccountPassword $LCPassword -Enabled $TRUE
+
+    }
+
+Import-ecorp -OUName TICSupport
+Import-ecorp -OUName E-CorpManagers
+Import-ecorp -OUName SalesMarketing
+Import-ecorp -OUName RoomService 
